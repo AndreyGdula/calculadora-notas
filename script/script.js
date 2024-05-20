9a48807d2e3f3d95cdccdc24e4664a459563ce49
AndreyGdula <andreygdula@gmail.com>
2024-05-20 00:16:25 -0300

    v1.0.1

--- script/script.js
+++ script/script.js
@@ -1,14 +1,24 @@
 function calcular() {
     const inputNota1 = document.querySelector("input#nota1")
     const inputNota2 = document.querySelector("input#nota2")
+    const divFinal = document.querySelector("div.final")
+    const inputFinal = document.querySelector("input#nota-final")
 
     const nota1 = Number((inputNota1).value)
-    const nota2 = Number((inputNota1).value)
+    const nota2 = Number((inputNota2).value)
 
-    const notaFinal2 = (10 - (2 / 3) * nota1).toFixed(1)
+    if (!nota1) {
+        alert("Erro! Nota 1 não foi informado!")
+    } else if (nota1 > 10 || nota2 > 10) {
+        alert("Erro! A nota mencionada é maior que 10. Tente novamente!")
+    } else if (!nota2) {
+        divFinal.style.display = "none"
+        const notaFinal2 = (10 - (2 / 3) * nota1).toFixed(1)
+        inputNota2.placeholder = `${notaFinal2.toString().replace('.', ',')}`
+    } else {
+        const notaFinal = ((nota1 * 0.4) + (nota2 * 0.6)).toFixed(1)
 
-    inputNota2.placeholder = `${notaFinal2}`
-    inputNota2.style.textAlign = "center"
-    inputNota1.style.textAlign = "center"
-    
+        divFinal.style.display = "grid"
+        inputFinal.placeholder = `${notaFinal}`
+    }
 }
